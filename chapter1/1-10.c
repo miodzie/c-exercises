dd4436993882ff551ef083bae6a5fc8d2126eb06
miodzie <miodzie@protonmail.com>
2022-06-11 16:58:00 -0400

    Exercise 1-10

--- chapter1/1-10.c
+++ chapter1/1-10.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
+char replace_escapes(char c, char escape, char replace) {
+  if (c == escape) {
+      putchar('\\');
+      return replace;
+  }
+
+  return c;
+}
+
 int main() {
   int c;
 
   while((c = getchar()) != EOF) {
-    if (c == '\t') {
-      putchar('\\');
-    }
+    c = replace_escapes(c, '\t', 't');
+    c = replace_escapes(c, '\b', 'b');
+    c = replace_escapes(c, '\\', '\\');
+
     putchar(c);
   }
 }
