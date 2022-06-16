649ff210576aa87eb3508fdac1bfca05b1e29286
miodzie <miodzie@protonmail.com>
2022-06-16 02:22:13 -0400

    Section 1.7 Functions. Exercise 1-15

---
+++ chapter1/1-15.c
@@ -0,0 +1,17 @@
+// Rewrite the temperature conversion program of Section 1.2 to use a function
+// for conversion.
+
+#include <stdio.h>
+
+float fahr_to_celsius(int);
+
+int main() {
+  int fahr;
+
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf("%3d %6.1f\n", fahr, fahr_to_celsius(fahr));
+}
+
+float fahr_to_celsius(int fahr) {
+  return (5.0 / 9.0) * (fahr-32);
+}
